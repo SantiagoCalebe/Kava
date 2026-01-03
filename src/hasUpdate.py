a2684ee6bbd9739918df984c9496c21020a129b6
Santiago <calebesantiago206@gmail.com>
2026-01-03 12:40:05 -0300

    lil'

--- src/hasUpdate.py
+++ src/hasUpdate.py
@@ -1,12 +1,11 @@
-def _parse(v):
-    return tuple(map(int, v.split(".")))
+import urllib.request
+from version import KavaVersion
 
 def hasUpdate():
     VERSION_URL = "https://raw.githubusercontent.com/SantiagoCalebe/Kava/refs/heads/main/gitVersion.txt"
-
     try:
         with urllib.request.urlopen(VERSION_URL) as r:
-            remote = r.read().decode().strip()
-            return _parse(remote) > _parse(KavaVersion.VERSION)
+            remote_version = r.read().decode("utf-8").strip()
+            return remote_version != str(KavaVersion)
     except:
         return False
